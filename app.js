f03be392cf19ec8e7b1e669303f85387ad25131b
krishna mangal <erkrishnamangal40@gmail.com>
2025-02-17 22:57:22 +0530

    17th class

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express=require('express');
 const app=express();
 app.use('/test/1',(res,req)=>{
+    console.log(req.query);
     req.send('Hello helo helo');
 })
-app.use('/test',(res,req)=>{
-    req.send('Hello World');
+// app.use('/test',(res,req)=>{
+//     req.send('Hello World');
+// })
+app.get('/testing/:userid',(res,req)=>{
+    console.log(req.params);
+    req.send({helooo:"krishnamanagal"});
+})
+app.post('/test*ing',(res,req)=>{
+    req.send({helooo:"krishnamanagalji"});
+})
+app.get(/.*ji$/,(res,req)=>{
+    req.send({helooo:"krishnamanagaljiiii"});
 })
 
-app.use((res,req)=>{
-    req.send('Hello ji');//below this are also same as this and this will override below code
-})
-app.use("/",(req,res)=>{
-    res.send('Hello Worldji');
-})
+// app.use((res,req)=>{
+//     req.send('Hello ji');//below this are also same as this and this will override below code
+// })
+// app.use("/",(req,res)=>{
+//     res.send('Hello Worldji');
+// })
 app.listen(3000,()=>{console.log('Server is running on port 3000')});
