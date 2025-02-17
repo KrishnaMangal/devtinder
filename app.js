f7e4a1f7f29758a5f587ce2c72fa4a969382120a
krishna mangal <erkrishnamangal40@gmail.com>
2025-02-17 22:56:32 +0530

    16 class

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
+app.use('/test/1',(res,req)=>{
+    req.send('Hello helo helo');
+})
 app.use('/test',(res,req)=>{
     req.send('Hello World');
 })
+
 app.use((res,req)=>{
-    req.send('Hello ji');
+    req.send('Hello ji');//below this are also same as this and this will override below code
+})
+app.use("/",(req,res)=>{
+    res.send('Hello Worldji');
 })
 app.listen(3000,()=>{console.log('Server is running on port 3000')});
